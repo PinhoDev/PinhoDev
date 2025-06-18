7ddc4505547096b8785c53ef217327cd29e99dec
Andre Pinho <122530757+PinhoDev@users.noreply.github.com>
2025-06-18 22:18:47 +0200

    Update web page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
